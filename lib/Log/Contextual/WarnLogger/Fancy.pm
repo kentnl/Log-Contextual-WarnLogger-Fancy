7991fe11d4c7ca8710ccb4e46b4e411b6b2c66ce
Kent Fredric <kentfredric@gmail.com>
2016-05-15 13:51:12 +1200

    Document features

--- lib/Log/Contextual/WarnLogger/Fancy.pm
+++ lib/Log/Contextual/WarnLogger/Fancy.pm
@@ -6,33 +6,7 @@ package Log::Contextual::WarnLogger::Fancy;
 
 our $VERSION = '0.001000';
 
-# This class provides a few patterns not in the Log::Contextual::Easy::Default
-# and Log::Contextual::WarnLogger features.
-#
-# 1. Has a shared prefix for all compontents that use it, which is used
-#    when the per-package prefix is omitted. This is mostly just to simplify "All on"
-#    and "All off" behaviours will still allowing fine-grained control for precision tracing.
-#
-# 2. Has a "default" upto level of "warn", so that warn levels and higher can be used like
-#    normal warnings and be user visible.
-#
-# 3. Has a "logger label" which provides a compacted module name that is infix-compacted
-#    to 21 characters to make flow more obvious in conjunction with the "shared prefix"
-#    option.
-#
-# 4. Has ANSI Color tinting of log messages for easy skimming, which incidentally makes any app
-#    that uses this look much more modern.
-#
-# The biggest downside of doing this has been the details are so specific that I had to
-#    rewrite all the existing logic to support it :/
-#
-# Also, this class intentionally nuked all "custom levels" support to keep the complexity
-# and to avoid AUTOLOAD shenianigans.
-
-use strict;
-use warnings;
 use Carp qw( croak );
-
 use Term::ANSIColor qw( colored );
 
 delete $Log::Contextual::WarnLogger::Fancy::{$_}
@@ -73,7 +47,7 @@ sub new {
     my $self = bless {}, $class;
 
     $self->{env_prefix} = $args->{env_prefix}
-      or die 'no env_prefix passed to ' . __PACKAGE__ . '->new';
+      or croak 'no env_prefix passed to ' . __PACKAGE__ . '->new';
 
     for my $field (qw( group_env_prefix default_upto label label_length )) {
         $self->{$field} = $args->{$field} if exists $args->{$field};
@@ -188,12 +162,13 @@ sub _gen_is_level_sub {
             croak "Unrecognized log level '$upto' in \$ENV{$gp_upto}"
               if not defined $self->{level_nums}->{$upto};
         }
-        elsif ( exists $self->{default_upto} ) {
+        elsif ( defined $self->{default_upto} ) {
             $upto = $self->{default_upto};
         }
         else {
             return 0;
         }
+
         return $self->{level_nums}->{$level} >= $self->{level_nums}->{$upto};
     };
 }
@@ -217,9 +192,152 @@ sub _gen_level {
 
 __END__
 
+=encoding UTF-8
+
 =head1 NAME
 
 Log::Contextual::WarnLogger::Fancy - A modernish default lightweight logger
+
+=head1 DESCRIPTION
+
+Log::Contextual::WarnLogger::Fancy is a re-implementation of L<Log::Contextual::WarnLogger> with a few conveniences added.
+
+=over 4
+
+=item * L<Group Prefixes|/Group Prefixes>
+
+=item * L<< Default C<upto> level|/Default upto level. >>
+
+=item * L<ANSI Colors|/ANSI Colors>
+
+=item * L<< Padded Log Level Labels|/Padded Log Level Labels >>
+
+=item * L<< Per Logger Origin Labels|/Per Logger Origin Labels >> with padding/shortening.
+
+=back
+
+=head1 FEATURES NOT IN C<::WarnLogger>
+
+=head2 Group Prefixes
+
+  package My::Project::Package;
+
+  my $logger = Log::Contextual::WarnLogger::Fancy->new(
+    env_prefix       => 'MY_PROJECT_PACKAGE',   # control this level only
+    group_env_prefix => 'MY_PROJECT',           # shared control for all components
+  );
+
+This allows adding targeted granular debug tracing to selected packages within a project,
+or within all packages in a project, without having to resort to heavyweight loggers.
+
+  MY_PROJECT_UPTO=debug           # debug the whole project
+  MY_PROJECT_PACKAGE_UPTO=trace   # trace level on ::Package
+
+=head2 Default C<upto> level.
+
+Because it seems like you'd want to make C<log_warn> and C<log_fatal> visible on the controlling terminal,
+its useful not to need to set C<MY_PROJECT_UPTO=> in your environment just to get some useful output.
+
+Instead, by default, C<::Fancy> assumes a default C<upto> value of C<warn>, and you can adjust this via
+
+  my $logger = Log::Contextual::WarnLogger::Fancy->new(
+    ...
+    default_upto     => 'info',
+  );
+
+And from then on, you can use C<log_warn { ... }> with the same convenience as C<warn ...> and know that
+your user will see it, and that if they bolt on some advanced logging system, that those warnings will flow
+nicely into it.
+
+=head2 ANSI Colors
+
+In order to make log output faster to visually scan, ANSI Colour codes are used to indicate various log levels.
+
+  trace => plain
+  debug => blue
+  info  => white
+  warn  => yellow
+  error => magenta
+  fatal => red
+
+=head2 Padded Log Level Labels
+
+Because some of us are anal as hell, the following just looks messy:
+
+  [trace] message
+  [info] message
+  [fatal] message
+
+So C<level> labels are constantly padded to 5 characters.
+
+  [trace] message
+  [info ] message  # Aaaaah. (~˘▾˜)/
+  [fatal] message
+
+=head2 Per Logger Origin Labels
+
+Because the recommended usage of C<::WarnLogger::Fancy> results in creating
+a logger instance per package, each logger instance can be customized with a
+prefix to give more context when used in bulk.
+
+  my $logger = Log::Contextual::WarnLogger::Fancy->new(
+    label         => __PACKAGE__
+    label_length  => 16,
+  );
+
+...
+
+  [trace    Some::Package] A trace message
+  [info    Other::Package] An info message
+  [warn     Some::Package] A warning
+  [fatal My::Oth¦:Package] A fatal warning
+
+Additionally, Origin Labels are constant-sized where specified, defaulting to
+C<16> characters long, either left-padded to meet that length, or C<infix>
+condensed to C<16> characters.
+
+Currently, the C<infix> character of choice is C<¦> ( C<chr(166)> U+00A6 BROKEN BAR )
+which may be superficially similar to C<|> ( C<chr(124)> U+007C VERTICAL LINE )
+
+Chosen as such because its the only character I could find that
+
+=over 4
+
+=item * Is unlikely to be in common usage
+
+=item * Will be visually distinct
+
+=item * Will be likely to be understood in its context without elaborate explanation
+
+=item * Will likely render as intended on all output terminals
+
+=item * Won't require magic with turning on UTF8 Support
+
+=back
+
+C<¦> is the same in both C<UTF-8>, C<ISO-8859-1> and C<Windows-1252>, with only C<CP437> being an outlier
+with C<FEMININE ORDINAL INDICATOR> ( Equiv of U+00AA )
+
+I'd have ideally used C<…> ( U+2026 HORIZONTAL ELLIPSIS ), but getting C<UTF-8> magic is filled with challenges.
+
+( It however still might be implemented this way in future )
+
+=head1 INTENTIONALLY MISSING FEATURES
+
+=head2 Custom Logging Levels
+
+C<::WarnLogger> has one feature that adds a bit of complexity, and there's additional
+implementation confusion for me in mapping/understanding the existing code.
+
+Specifically with regards to C<AUTOLOAD> and how it implements custom logging levels
+differently from stock levels.
+
+The plumbing is in place to implement it in a similar way, but its not clear to me if
+this feature is a "Good idea" or not at this time.
+
+So the glue is in place for people who are sure they need it, but its intentionally obscure
+to serve as a disincentive. ( Please file a bug/contact me somehow if you are sure this is a good idea,
+preferably with explanations on how you plan on using it with example code )
 
 =head1 AUTHOR
 
